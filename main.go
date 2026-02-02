222377184e8428824e6298ef445a6f0b986f768b
lfln3 <lfln3@@>
2026-02-02 14:05:40 +0300

    done

--- main.go
+++ main.go
@@ -6,18 +6,132 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
 	"time"
 )
 
 type Stat struct {
 	avgResponseTime int64
-	statusCodes map[int]int
-	respQty int64
-	mutex sync.Mutex
+	statusCodes     map[int]int
+	respQty         int64
+	mutex           sync.Mutex
 }
 
-func repeatAction(domain string, m_map map[string]Stat) error {
+func constructStatuses() *Stat {
+	return &Stat{
+		statusCodes: make(map[int]int),
+	}
+}
+
+type GStat struct {
+	items map[string]*Stat
+	mu    sync.RWMutex
+}
+
+type Monitor struct {
+	domains[] string
+	gs *GStat
+	done chan struct{}
+}
+
+func newMonitor(domains[] string) *Monitor {
+	gs := &GStat{
+		items: make(map[string]*Stat, len(domains)),
+	}
+	for _, name := range domains {
+		gs.items[name] = constructStatuses()
+	}
+	done := make(chan struct{})
+
+	return &Monitor{
+		domains: domains,
+		gs: gs,
+		done: done,
+	}
+}
+
+func (m *Monitor) displayDomains() {
+	fmt.Println(strings.Repeat("=", 20) + "List of domains" + strings.Repeat("=", 20))
+	for ind, name := range m.domains {
+		fmt.Printf("%d --- %s\n", ind, name)
+	}
+	fmt.Println(strings.Repeat("=", 55))
+}
+
+func (m *Monitor) start(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				for _, val := range m.domains {
+					go repeatAction(val, m.gs)
+				}
+			case <-m.done:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+}
+
+func (m *Monitor) stop() {
+	close(m.done)
+}
+
+func (m *Monitor) printSummary() {
+	fmt.Println("\n" + strings.Repeat("=", 55))
+	fmt.Printf("%-20s | %-12s | %-8s | %-15s\n", "DOMAIN", "AVG TIME", "REQS", "STATUS CODES")
+	fmt.Println(strings.Repeat("-", 55))
+
+	for domain, s := range m.gs.items {
+		avg := s.avgResponseTime
+		qty := s.respQty
+
+		codes := ""
+		for code, count := range s.statusCodes {
+			codes += fmt.Sprintf("%d:%d ", code, count)
+		}
+
+
+		fmt.Printf("%-20s | %-10dms | %-8d | %-15s\n", domain, avg, qty, codes)
+	}
+	fmt.Println(strings.Repeat("=", 55))
+}
+
+type FileOpenError struct {
+	fname string
+}
+
+func (foe FileOpenError) Error() string {
+	return fmt.Sprintf("File open error on file - %s", foe.fname)
+}
+
+func loadDomains(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, &FileOpenError{fname: filename}
+	}
+	defer file.Close()
+	var domains []string
+
+	reader := bufio.NewScanner(file)
+
+	for reader.Scan() {
+		line := reader.Text()
+		if line == "" {
+			continue
+		}
+		domains = append(domains, line)
+	}
+	return domains, nil
+}
+
+
+
+func repeatAction(domain string, gstat *GStat) error {
 	start := time.Now()
 	resp, err := http.Get(domain)
 	end := time.Now()
@@ -27,71 +141,46 @@ func repeatAction(domain string, m_map map[string]Stat) error {
 
 	defer resp.Body.Close()
 
-	m_map[domain].mutex.Lock()
-	rTime := end.Sub(start).Milliseconds();
-	m_map[domain].avgResponseTime = (m_map[domain].avgResponseTime * m_map[domain].respQty + rTime) / (m_map[domain].respQty + 1) 
-	m_map[domain].statusCodes[resp.StatusCode] += 1
-	m_map[domain].respQty++
-	m_map[domain].mutex.Unlock()
+	gstat.mu.RLock()
+	s := gstat.items[domain]
+	gstat.mu.RUnlock()
+
+	s.mutex.Lock()
+	rTime := end.Sub(start).Milliseconds()
+	s.avgResponseTime = (s.avgResponseTime*s.respQty + rTime) / (s.respQty + 1)
+	s.statusCodes[resp.StatusCode] += 1
+	s.respQty++
+	s.mutex.Unlock()
+
 
 	fmt.Printf("domain request for %s, status code - %d\n", domain, resp.StatusCode)
 	fmt.Printf("response time - %dms\n", rTime)
 	return nil
 }
 
-
-func summary(stats []Stat) {
-
-}
-
-func main() {
-	filename := "domains.txt"
-	file, err := os.Open(filename)
-
-	if err != nil {
-		fmt.Println("err open file")
-		return;
-	}
-	defer file.Close()
-	var domains []string
-	
-	reader := bufio.NewScanner(file)
-
-	for reader.Scan() {
-		line := reader.Text()
-		if (line == "") {
-			continue
-		}
-		domains = append(domains, line)
-	}
-	
-	var statsPerDomain map[string]Stat = make(map[string] Stat, len(domains))
-
-	for ind, name := range(domains) {
-		fmt.Printf("%d --- %s\n", ind, name)
-	}
-
-	interval := time.Second * 1;
-	ticker := time.NewTicker(interval)
-	done := make(chan bool)
-
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				for _, val := range(domains) {
-					go repeatAction(val, statsPerDomain)
-				}
-			case <-done:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
-
+func handleSignals() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
 	<-c
-	done<-true
+}
+
+func main() {
+	filename := "domains.txt"
+	domains, err := loadDomains(filename)
+
+	if (err != nil) {
+		fmt.Printf("%s", err)
+		return;
+	}
+
+	monitor := newMonitor(domains)
+	monitor.displayDomains()
+	interval := time.Second * 1
+
+	monitor.start(interval)
+
+	handleSignals()
+	monitor.stop()
+	monitor.printSummary()
 }
